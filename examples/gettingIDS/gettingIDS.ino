6a2bde5f8d41a3d89fa048153d23c32d89d72fcd
Jo4096 <jcarlos.rebelo.lopes2004@gmail.com>
2024-08-23 15:34:38 +0100

    Update gettingIDS.ino

--- examples/gettingIDS/gettingIDS.ino
+++ examples/gettingIDS/gettingIDS.ino
@@ -3,7 +3,6 @@
 void setup()
 {
     INIT_SERIAL_COM();   // macro to init the Serial with the correct baud
-    Serial1.begin(9600); // I use Serial1 to communicate with the pc in order to print the id
 }
 
 void loop()
@@ -11,18 +10,17 @@ void loop()
     ServoController servoCont(Serial);
     uint8_t id = 0;
 
-    if (servoCont.getID(&id)) // dont chain servos when using this function. It will create a bus conflict since the function calls all servos to respond
-    {
-        Serial1.println(id);
-    }
-    else
-    {
-        Serial1.println(F("Something went wrong..."));
-    }
-
     while (true)
     {
-        servoCont.changeLed(NEW_ID); // blinking the built-in LED from the servo
-        servoCont.waitFor(1000);
+        if (servoCont.getID(&id)) // dont chain servos when using this function. It will create a bus conflict since the function calls all servos to respond
+        {
+            Serial.println(id);
+        }
+        else
+        {
+            Serial.println(F("Something went wrong..."));
+        }
+
+        servoCont.waitFor(2000);
     }
 }
